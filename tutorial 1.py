f1acbbbff3081dc957a6aeb2d54e515dee488ecf
Anuprathasan <75056034+Anuprathasan@users.noreply.github.com>
2021-06-14 01:21:52 +0800

    Add files via upload

---
+++ tutorial 1.py
@@ -0,0 +1,12 @@
+import cv2
+
+img = cv2.imread('assets/rainbow.png', 1)
+img = cv2.resize(img, (0,0), fx=2, fy=2)
+img = cv2.rotate(img, cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+cv2.imwrite('assets/new_img_.png', img)
+
+cv2.imshow('Image', img)
+cv2.waitKey(0 )
+cv2.destroyAllWindows(0)
+ 
