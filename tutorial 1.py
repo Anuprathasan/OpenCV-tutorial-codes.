531e069fd94dcf47e754b3456566fed88fe0eb59
Anuprathasan <75056034+Anuprathasan@users.noreply.github.com>
2021-06-14 23:06:55 +0800

    Update tutorial 1.py

--- tutorial 1.py
+++ tutorial 1.py
@@ -2,7 +2,7 @@ import cv2
 
 img = cv2.imread('assets/rainbow.png', 1)
 img = cv2.resize(img, (0,0), fx=2, fy=2)
-img = cv2.rotate(img, cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
+img = cv2.rotate(img, cv2.cv2.ROTATE_90_CLOCKWISE)
 
 cv2.imwrite('assets/new_img_.png', img)
 
