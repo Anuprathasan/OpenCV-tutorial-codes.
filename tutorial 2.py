6e77fafb3c7ad3abc46727b327c7d8b2675ea117
Anuprathasan <75056034+Anuprathasan@users.noreply.github.com>
2021-06-22 23:16:32 +0800

    Update tutorial 2.py

--- tutorial 2.py
+++ tutorial 2.py
@@ -1,16 +1,17 @@
 import cv2
 import random
 
-img = cv2.imread('assets/rainbow.png', -1)
-
+img = cv2.imread('assets/nature.jpg', -1)
+print (img.shape)
 # Change first 100 rows to random pixels
 for i in range(100):
 	for j in range(img.shape[1]):
-		img[i][j] = [random.randint(225, 255), random.randint(225, 255), random.randint(0, 255)]
+		img[i][j] = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
 
 # Copy part of image
-tag = img[500:700, 600:900]
-img[100:300, 650:950] = tag
+tag = img[150:320, 250:350]
+print (tag.shape)
+img[10:180, 50:150] = tag
 
 cv2.imshow('Image', img)
 cv2.waitKey(0)
